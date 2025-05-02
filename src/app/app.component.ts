1b2b73841cd5ffb3c17728ebaa65eadecd825b7c
Patricio Alejandro Guevara Olaez <85137144+PGO-Alex@users.noreply.github.com>
2025-05-02 07:16:27 -0600

    added components card, home and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'innovatube-fr';
+  title = 'innovatube';
+  site_name = 'innovatube'
 }
